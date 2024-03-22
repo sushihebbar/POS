a73d0a7c2acdc623a8964960ee19ca8a37e9ef6f
sushihebbar <sushi.hebbar@gmail.com>
2024-03-23 00:15:23 +0530

    intial

---
+++ store-pos/src/database/DbConnection.java
@@ -0,0 +1,40 @@
+/**
+ * @author inforkgodara
+ */
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DbConnection {
+    private Connection con;
+    private static DbConnection dbc;
+    private DbConnection() {
+        try {
+            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+            con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","POS","POS");
+//              con = DriverManager.getConnection("jdbc:mysql://localhost:3306/database_name", "username", "password");
+
+        } catch (SQLException ex) {
+            Logger.getLogger(DbConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static DbConnection getDatabaseConnection() {
+        if (dbc == null) {
+            dbc = new DbConnection();
+        }
+        return dbc;
+    }
+    
+    public Connection getConnection() {
+        return con;
+    }
+    
+    public static void main(String[] args) {
+        new DbConnection();
+    }
+}
